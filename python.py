e4ed3cb58c55484114e2cd06ca472733e7b02f14
VanajaRed <91769995+VanajaRed@users.noreply.github.com>
2021-10-02 09:13:57 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+import sqlite3
+conn=sqlite3.connect("movies.db")
+c=conn.cursor()
+conn.commit()
+conn.close()
+c.execute("create table favmovies(name text,actor text,actress text,director text,year_of_release integer""")
+rows=[('Thimmarusu','Satyadev kancharana','Priyanka Jawalkar','Sharan koppisetty',2021),
+('Narappa','Venkatesh','Priyamani','Srikanth addala',2021),
+('Paagal','Vishwak Sen','Nivetha Pethuraj','Naresh Kuppili',2021)]
+c.executemany('insert into favmovies values(?,?,?,?,?)',rows)
+c.execute("select * from favmovies")
+rec=c.fetchall()
+print(rec)
+conn.commit()
+conn.close()
