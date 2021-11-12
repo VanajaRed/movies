919dd03fa3f9765c7addb0a0bd9acb2c63d80c1c
VanajaRed <91769995+VanajaRed@users.noreply.github.com>
2021-11-12 10:41:55 +0530

    Update favmovies.py

--- favmovies.py
+++ favmovies.py
@@ -1,10 +1,13 @@
 import sqlite3
 conn=sqlite3.connect("movies.db")
 c=conn.cursor()
-c.execute("create table favmovies(name text,actor text,actress text,director text,year_of_release integer""")
+c.execute("""create table favmovies(name text,actor text,actress text,director text,year_of_release integer)""")
 rows=[('Thimmarusu','Satyadev kancharana','Priyanka Jawalkar','Sharan koppisetty',2021),
 ('Narappa','Venkatesh','Priyamani','Srikanth addala',2021),
-('Paagal','Vishwak Sen','Nivetha Pethuraj','Naresh Kuppili',2021)]
+('Paagal','Vishwak Sen','Nivetha Pethuraj','Naresh Kuppili',2021),
+('Jaanu','Sharwanand','Samantha Akkineni','C.Prem Kumar',2020),
+('Hit','Vishwak Sen','Ruhani Sharma','Sailesh Kolanu,2020),
+('V','Nani','Nivetha Thomas','Mohana Krishna',2020)]
 c.executemany('insert into favmovies values(?,?,?,?,?)',rows)
 c.execute("select * from favmovies")
 rec=c.fetchall()
